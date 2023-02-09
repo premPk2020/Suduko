36e2e159b0ce9b3f91f9a04230de2e43854ae751
Premkumar <premorange2020@gmail.com>
2023-02-09 19:02:03 +0530

    initial project created

---
+++ app/src/main/java/com/orange/sudoku/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.orange.sudoku
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.orange.sudoku.ui.theme.SudokuTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SudokuTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
